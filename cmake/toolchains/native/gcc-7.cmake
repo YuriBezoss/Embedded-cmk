8caf4df7a89b4ca00184557fb7b1fa39bf249129
home office <caokhoa.le@roundsolutions.com>
2021-08-21 22:40:42 +0200

    008-multiple-os

---
+++ cmake/toolchains/native/gcc-7.cmake
@@ -0,0 +1,8 @@
+###################
+# GCC-7 Toolchain #
+###################
+
+set(CMAKE_C_COMPILER    gcc-7)
+set(CMAKE_CXX_COMPILER  g++-7)
+# You could also use 'ar' from binutils
+set(CMAKE_AR            gcc-ar-7)
---
+++ cmake/toolchains/native/gcc-8.cmake
@@ -0,0 +1,8 @@
+###################
+# GCC-8 Toolchain #
+###################
+
+set(CMAKE_C_COMPILER    gcc-8)
+set(CMAKE_CXX_COMPILER  g++-8)
+# You could also use 'ar' from binutils
+set(CMAKE_AR            gcc-ar-8)
