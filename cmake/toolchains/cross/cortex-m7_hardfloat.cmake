6f4a8d10bf2cafa09890e0dcdeec59c51a4788ac
home office <caokhoa.le@roundsolutions.com>
2021-09-19 16:12:37 +0200

    011-cmake_tooling

--- cmake/toolchains/cross/cortex-m7_hardfloat.cmake
+++ cmake/toolchains/cross/cortex-m7_hardfloat.cmake
@@ -11,8 +11,10 @@ endif()
 
 set(ARM_CORTEX_M7_HARDFLOAT_TOOLCHAIN_INCLUDED true)
 
-set(CPU_NAME cortex-m7)
-set(CPU_FLAGS "-mcpu=cortex-m7 -mthumb")
-set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
+if(NOT CPU_NAME)
+	set(CPU_NAME cortex-m7)
+endif()
+set(CPU_FLAGS "-mcpu=cortex-m7 -mthumb ${CPU_FLAGS}")
+set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16 ${VFP_FLAGS}")
 
 include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi-gcc.cmake)
